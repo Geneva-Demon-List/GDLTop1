4b4d59144885afbb522e43fa9e9bbda0508939c1
Rocketpowered-tech <chaserob12345@gmail.com>
2026-02-11 18:49:31 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -121,6 +121,9 @@ export default {
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
                     </p>
                     <p>
+                        The recording must have audible click sounds OR a handcam or your record will not be accepted.
+                    </p>
+                    <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
